3180c97736eb2578178b220ad2ac2d4087bf7e1f
thar <itsmehchannel@gmail.com>
2023-12-21 04:42:19 -0800

    Changed navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,14 +3,14 @@ function Navbar() {
     <>
       <div class="navbar">
             <div class="left-wrapper">
-                <h1>ZotScheduler</h1>
+                <h1 class="white">ZotScheduler</h1>
             </div>
             <div class="right-wrapper">
                 <ul>
-                    <li><a href="" onClick='myFunction()'>Save</a></li>
-                    <li><a href="" onClick='myFunction()'>Load</a></li>
-                    <li><a href="" onClick='myFunction()'>Info</a></li>
-                    <li><a href="" onClick='myFunction()'>Settings</a></li>
+                    <li><button onClick='myFunction()'>Save</button></li>
+                    <li><button onClick='myFunction()'>Load</button></li>
+                    <li><button onClick='myFunction()'>Info</button></li>
+                    <li><button onClick='myFunction()'>Settings</button></li>
                 </ul>
             </div>
       </div>
