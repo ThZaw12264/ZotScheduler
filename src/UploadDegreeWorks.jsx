a00c75724aa8f1e7287c1841fa14970aa8743cb6
thar <itsmehchannel@gmail.com>
2023-12-23 17:58:39 -0800

    Mantine dropzone

--- src/UploadDegreeWorks.jsx
+++ src/UploadDegreeWorks.jsx
@@ -1,57 +1,62 @@
-import React, { useState } from "react";
+import { useRef } from 'react';
+import { Text, Group, Button, rem, useMantineTheme } from '@mantine/core';
+import { Dropzone, MIME_TYPES } from '@mantine/dropzone';
+import { IconCloudUpload, IconX, IconDownload } from '@tabler/icons-react';
+import classes from './UploadButton.module.css';
 
-const DegreeWorksUploader = () => {
-  const [status, setStatus] = useState();
-
-  const handleUpload = async (e) => {
-    if (e.target.files) {
-      const file = e.target.files[0];
-      setStatus("parsing");
-
-      const formData = new FormData();
-      formData.append("file", file);
-
-      try {
-        const result = await fetch("https://httpbin.org/post", {
-          method: "POST",
-          body: formData,
-        });
-
-        const data = await result.json();
-
-        console.log(data);
-        setStatus("success");
-      } catch (error) {
-        console.error(error);
-        setStatus("fail");
-      }
-    }
-  };
+const DegreeWorksUploader = ({ parsingStatus, handleUpload }) => {
+  const theme = useMantineTheme();
+  const openRef = useRef(null);
 
   return (
     <>
-      <div className="upload-group">
-        <label htmlFor="file">
-          Upload your DegreeWorks in PDF format: 
-        </label>
-        <input id="file" type="file" accept="application/pdf" onChange={handleUpload} />
-      </div>
+      <div className={classes.wrapper}>
+        <Dropzone
+          openRef={openRef}
+          onDrop={(file) => {handleUpload(file[0])}}
+          className={classes.dropzone}
+          radius="md"
+          accept={[MIME_TYPES.pdf]}
+          maxSize={30 * 1024 ** 2}
+        >
+          <div style={{ pointerEvents: 'none' }}>
+            <Group justify="center">
+              <Dropzone.Accept>
+                <IconDownload
+                  style={{ width: rem(50), height: rem(50) }}
+                  color={theme.colors.blue[6]}
+                  stroke={1.5}
+                />
+              </Dropzone.Accept>
+              <Dropzone.Reject>
+                <IconX
+                  style={{ width: rem(50), height: rem(50) }}
+                  color={theme.colors.red[6]}
+                  stroke={1.5}
+                />
+              </Dropzone.Reject>
+              <Dropzone.Idle>
+                <IconCloudUpload style={{ width: rem(50), height: rem(50) }} stroke={1.5} />
+              </Dropzone.Idle>
+            </Group>
 
-      <Result status={status} />
+            <Text ta="center" fw={700} fz="lg" mt="md">
+              <Dropzone.Accept>Drop file here</Dropzone.Accept>
+              <Dropzone.Reject>Must be a PDF with 30MB or less</Dropzone.Reject>
+              <Dropzone.Idle>Upload DegreeWorks</Dropzone.Idle>
+            </Text>
+            <Text ta="center" fz="sm" mt="xs" c="dimmed">
+              Drag and drop your DegreeWorks PDF file here to upload. Must be 30MB or less.
+            </Text>
+          </div>
+        </Dropzone>
+
+        <Button className={classes.control} size="md" radius="xl" onClick={() => openRef.current?.()}>
+          Select file
+        </Button>
+      </div>
     </>
   );
-};
-
-const Result = ({status}) => {
-  if (status === "success") {
-    return <p>✅ File parsed successfully!</p>;
-  } else if (status === "fail") {
-    return <p>❌ File parse failed!</p>;
-  } else if (status === "parsing") {
-    return <p>⏳ Parsing selected file...</p>;
-  } else {
-    return null;
-  }
 };
 
 export default DegreeWorksUploader;
