4853828531895eb0ddd135a1608e4e113eaf374a
thar <itsmehchannel@gmail.com>
2023-12-23 17:56:02 -0800

    Fixed RightPane

--- src/RightPane.jsx
+++ src/RightPane.jsx
@@ -1,16 +1,51 @@
 import { useState } from 'react'
 import DegreeWorksUploader from './UploadDegreeWorks'
 
-function LeftPane() {
-  const [count, setCount] = useState(0)
+function RightPane() {
+  const [parsingStatus, setParsingStatus] = useState();
+
+  const handleUpload = async (file) => {
+    setParsingStatus("parsing");
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+      const result = await fetch("https://httpbin.org/post", {
+        method: "POST",
+        body: formData,
+      });
+
+      const data = await result.json();
+
+      console.log(data);
+      setParsingStatus("success");
+    } catch (error) {
+      console.error(error);
+      setParsingStatus("fail");
+    }
+  };
+
+  const Result = ({parsingStatus}) => {
+    if (parsingStatus === "success") {
+      return <p>✅ File parsed successfully!</p>;
+    } else if (parsingStatus === "fail") {
+      return <p>❌ File parse failed!</p>;
+    } else if (parsingStatus === "parsing") {
+      return <p>⏳ Parsing selected file...</p>;
+    } else {
+      return null;
+    }
+  };
 
   return (
     <>
-      <div class="pane">
-        <DegreeWorksUploader />
+      <div className="pane">
+        <DegreeWorksUploader parsingStatus={parsingStatus} handleUpload={handleUpload} />
+        <Result parsingStatus={parsingStatus} />
       </div>
     </>
   )
 }
 
-export default LeftPane
+export default RightPane
